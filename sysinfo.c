f7a3e3b8939b83acfef62bd65efd8f13ca2c854c
Tachera Sasi <tacherasasi@gmail.com>
2025-10-07 09:11:23 +0300

    refactor get_uptime function to accept OS parameter and implement OS-based logic

--- sysinfo.c
+++ sysinfo.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 
-int get_uptime(){ //NOTE: Assuming the user must be on a macintosh 
+int get_uptime(char os[]){ //NOTE: Assuming the user must be on a macintosh 
+    const osType = os;
+    switch (osType){
+    case 'MAC':
+        return get_uptime_mac();
+        break;
+    default:
+        break;
+    }
     return 0;
+}
+
+int get_uptime_mac(){
+
 }
