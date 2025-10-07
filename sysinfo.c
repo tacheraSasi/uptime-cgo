306c059f96e157a29cd4da21917f6acafc9d8776
Tachera Sasi <tacherasasi@gmail.com>
2025-10-07 09:17:43 +0300

    add OS-based logic to get_uptime function for Linux support

--- sysinfo.c
+++ sysinfo.c
@@ -1,18 +1,68 @@
 #include <stdio.h>
+#include <sys/sysctl.h>
+#ifdef __linux__
+#include <sys/sysinfo.h>
+#endif
 
 
-int get_uptime(char os[]){ //NOTE: Assuming the user must be on a macintosh 
+int get_uptime(char os[]){
     const osType = os;
     switch (osType){
     case 'MAC':
         return get_uptime_mac();
         break;
+    case 'LINUX':
+        return get_uptime_linux();
+        break;
     default:
+        printf("Unsupported OS: %s\n", osType);
+        return 0;
         break;
     }
     return 0;
 }
 
 int get_uptime_mac(){
+    int mib[2];
+    struct timeval boottime;
+    size_t size = sizeof(boottime);
+    time_t now;
+    time_t uptime;
 
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_BOOTTIME;
+
+    if (sysctl(mib, 2, &boottime, &size, NULL, 0) != 0) {
+        perror("sysctl");
+        return -1;
+    }
+
+    time(&now);
+    uptime = now - boottime.tv_sec;
+
+    int days = uptime / (24 * 3600);
+    uptime %= (24 * 3600);
+    int hours = uptime / 3600;
+    uptime %= 3600;
+    int minutes = uptime / 60;
+    int seconds = uptime % 60;
+
+    return sprintf("%d days, %d hours, %d minutes, %d seconds\n", days, hours, minutes, seconds);
+}
+
+int get_uptime_linux(){
+    struct sysinfo info;
+    if (sysinfo(&info) != 0) {
+        perror("sysinfo");
+        return -1;
+    }
+    long uptime = info.uptime;
+   12     int days = uptime / (24 * 3600);
+   13     uptime %= (24 * 3600);
+   14     int hours = uptime / 3600;
+   15     uptime %= 3600;
+   16     int minutes = uptime / 60;
+   17     int seconds = uptime % 60;
+
+    return sprintf("%d days, %d hours, %d minutes, %d seconds\n", days, hours, minutes, seconds);
 }
