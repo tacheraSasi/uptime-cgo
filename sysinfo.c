4a3aebe382629f7ddbb976c566e0a2bdf382a769
tacheraSasi <tacherasasi@gmail.com>
2025-12-14 01:46:40 +0300

    Update sysinfo.c

--- sysinfo.c
+++ sysinfo.c
@@ -1,106 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <sys/time.h>
 #include <sys/sysctl.h>
+#include <sys/time.h>
+#include <time.h>
 #ifdef __linux__
 #include <sys/sysinfo.h>
 #endif
 #include "sysinfo.h"
 
-char* get_uptime(char os[]){
-    if (strcmp(os, "MAC") == 0) {
-        return get_uptime_mac();
-    } else if (strcmp(os, "LINUX") == 0) {
-        return get_uptime_linux();
-    } else {
-        printf("Unsupported OS: %s\n", os);
-        return NULL;
-    }
+typedef struct {
+  int days;
+  int hours;
+  int minutes;
+  int seconds;
+} uptime_struct;
+
+char *get_uptime(char os[]) {
+  if (strcmp(os, "MAC") == 0) {
+    return get_uptime_mac();
+  } else if (strcmp(os, "LINUX") == 0) {
+    return get_uptime_linux();
+  } else {
+    printf("Unsupported OS: %s\n", os);
+    return NULL;
+  }
 }
 
-char* get_uptime_mac(){
-    int mib[2];
-    struct timeval boottime;
-    size_t size = sizeof(boottime);
-    time_t now;
-    time_t uptime;
+uptime_struct get_uptime_mac() {
+  int mib[2];
+  struct timeval boottime;
+  uptime_struct uptime_body;
+  size_t size = sizeof(boottime);
+  time_t now;
+  time_t uptime;
 
-    mib[0] = CTL_KERN;
-    mib[1] = KERN_BOOTTIME;
+  mib[0] = CTL_KERN;
+  mib[1] = KERN_BOOTTIME;
 
-    if (sysctl(mib, 2, &boottime, &size, NULL, 0) != 0) {
-        perror("sysctl");
-        return NULL;
-    }
+  if (sysctl(mib, 2, &boottime, &size, NULL, 0) != 0) {
+    perror("sysctl");
+    return NULL;
+  }
 
-    time(&now);
-    uptime = now - boottime.tv_sec;
+  time(&now);
+  uptime = now - boottime.tv_sec;
 
-    int days = uptime / (24 * 3600);
-    uptime %= (24 * 3600);
-    int hours = uptime / 3600;
-    uptime %= 3600;
-    int minutes = uptime / 60;
-    int seconds = uptime % 60;
+  int days = uptime / (24 * 3600);
+  uptime %= (24 * 3600);
+  int hours = uptime / 3600;
+  uptime %= 3600;
+  int minutes = uptime / 60;
+  int seconds = uptime % 60;
 
-    char* result = malloc(256);
-    if (result == NULL) {
-        return NULL;
-    }
-    char daysString[64];
-    if (days == 1) {
-        sprintf(daysString, "1 day");
-    } else {
-        sprintf(daysString, "%d days", days);
-    }
+  char *result = malloc(256);
+  if (result == NULL) {
+    return NULL;
+  }
+  char daysString[64];
+  if (days == 1) {
+    sprintf(daysString, "1 day");
+  } else {
+    sprintf(daysString, "%d days", days);
+  }
 
-    char hoursString[64];
-    if (hours == 1) {
-        sprintf(hoursString, "1 hour");
-    } else {
-        sprintf(hoursString, "%d hours", hours);
-    }
-    char minutesString[64];
-    if (minutes == 1) {
-        sprintf(minutesString, "1 minute");
-    } else {
-        sprintf(minutesString, "%d minutes", minutes);
-    }
-    char secondsString[64];
-    if (seconds == 1) {
-        sprintf(secondsString, "1 second");
-    } else {
-        sprintf(secondsString, "%d seconds", seconds);
-    }
-    char* uptime_str = malloc(256);
-    sprintf(uptime_str, "%s, %s, %s, %s\n", daysString, hoursString, minutesString, secondsString);
-    return uptime_str;
+  char hoursString[64];
+  if (hours == 1) {
+    sprintf(hoursString, "1 hour");
+  } else {
+    sprintf(hoursString, "%d hours", hours);
+  }
+  char minutesString[64];
+  if (minutes == 1) {
+    sprintf(minutesString, "1 minute");
+  } else {
+    sprintf(minutesString, "%d minutes", minutes);
+  }
+  char secondsString[64];
+  if (seconds == 1) {
+    sprintf(secondsString, "1 second");
+  } else {
+    sprintf(secondsString, "%d seconds", seconds);
+  }
+  char *uptime_str = malloc(256);
+  uptime_body.hours = hours;
+  uptime_body.minutes = minutes;
+  uptime_body.seconds = seconds;
+  return uptime_body;
 }
 
-char* get_uptime_linux(){
+uptime_struct get_uptime_linux() {
+  uptime_struct uptime_body;
 #ifdef __linux__
-    struct sysinfo info;
-    if (sysinfo(&info) != 0) {
-        perror("sysinfo");
-        return NULL;
-    }
-    long uptime = info.uptime;
-    int days = uptime / (24 * 3600);
-    uptime %= (24 * 3600);
-    int hours = uptime / 3600;
-    uptime %= 3600;
-    int minutes = uptime / 60;
-    int seconds = uptime % 60;
+  struct sysinfo info;
+  if (sysinfo(&info) != 0) {
+    perror("sysinfo");
+    return NULL;
+  }
+  long uptime = info.uptime;
+  int days = uptime / (24 * 3600);
+  uptime %= (24 * 3600);
+  int hours = uptime / 3600;
+  uptime %= 3600;
+  int minutes = uptime / 60;
+  int seconds = uptime % 60;
 
-    char* result = malloc(256);
-    if (result == NULL) {
-        return NULL;
-    }
-    sprintf(result, "%d days, %d hours, %d minutes, %d seconds\n", days, hours, minutes, seconds);
-    return result;
+  char *result = malloc(256);
+  if (result == NULL) {
+    return NULL;
+  }
+  sprintf(result, "%d days, %d hours, %d minutes, %d seconds\n", days, hours,
+          minutes, seconds);
+  uptime_body.days = days;
+  uptime_body.hours = hours;
+  uptime_body.minutes = minutes;
+  uptime_body.seconds = seconds;
+  return uptime_body;
 #else
-    return "Linux sysinfo not supported on this platform";
+  return uptime_body;
 #endif
 }
